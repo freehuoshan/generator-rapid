349c6a509ef5eee87b69cb2aaa2541d3418db28d
freehuoshan <free.huoshan@gmail.com>
2016-12-27 16:49:32 +0800

    rename template to dubbo_service_web_template

--- wangxj-generator-rapid/src/main/java/cn/org/rapid_framework/generator/GeneratorMain.java
+++ wangxj-generator-rapid/src/main/java/cn/org/rapid_framework/generator/GeneratorMain.java
@@ -17,7 +17,7 @@ public class GeneratorMain {
 		
 		g.deleteOutRootDir();							//删除生成器的输出目录
 //		g.generateByTable("table_name","template");	//通过数据库表生成文件,template为模板的根目录
-		g.generateByAllTable("template7");	//自动搜索数据库中的所有表并生成文件,template为模板的根目录
+		g.generateByAllTable("dubbo_service_web_template");	//自动搜索数据库中的所有表并生成文件,template为模板的根目录
 //		g.generateByClass(Blog.class,"template_clazz");
 		 
 //		g.deleteByTable("table_name", "template"); //删除生成的文件
